0ac2abcdd8f3bc0104b6a1b434246d9d9e8d47b4
Landon Soloway <quikkfreeze@gmail.com>
2020-03-28 12:48:51 -0500

    Built associations through the join table for Pokemon and Order.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 class Order < ApplicationRecord
-  belongs_to :customer_id
+  belongs_to :customer
+
+  has_many :pokemon_orders
+  has_many :pokemons, through: :pokemon_orders
 end
--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -2,4 +2,7 @@
 
 class Pokemon < ApplicationRecord
   has_and_belongs_to_many :types
+
+  has_many :pokemon_orders
+  has_many :orders, through: :pokemon_orders
 end
