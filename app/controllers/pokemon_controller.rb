cbf36c520229d44377a0a3a13ea261766277732d
Landon Soloway <quikkfreeze@gmail.com>
2020-03-30 11:26:05 -0500

    Built a search form and controller for searching pokemon.

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -8,4 +8,17 @@ class PokemonController < ApplicationController
   def show
     @pokemon = Pokemon.find(params[:id])
   end
+
+  def search
+    @pokemons = Pokemon.where('pokemons.name LIKE ?',
+                              "%#{params[:search_term]}%")
+
+    unless params[:selected_type].empty?
+      @type = Type.find_by(id: params[:selected_type])
+
+      @pokemons = pokemons.includes(:types)
+                          .where('types.id = ?', params[:selected_type])
+                          .references(:types)
+    end
+  end
 end
