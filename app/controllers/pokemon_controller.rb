629f82f1dfdbb57399a7a107bfc0ef3fbd3979b2
Landon Soloway <quikkfreeze@gmail.com>
2020-03-30 09:06:41 -0500

    Created a pokemon card partial in a shared folder for all views.

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 class PokemonController < ApplicationController
   def index
+    @pokemons = Pokemon.all.order(:dex_id)
   end
 
   def show
+    @pokemon = Pokemon.find(params[:id])
   end
 end
