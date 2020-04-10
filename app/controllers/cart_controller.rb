9641c8eb3368d8baa988c69262593dbaab733d72
Landon Soloway <quikkfreeze@gmail.com>
2020-04-10 13:06:28 -0500

    Made a show action and a sub total action in the cart controller.

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,4 +1,18 @@
+# frozen_string_literal: true
+
 class CartController < ApplicationController
   def show
+    @pokemons = Pokemon.find(session[:cart])
+
+    sub_total
+  end
+
+  private
+
+  def sub_total
+    @sub_total = 0
+    @pokemons.each do |pokemon|
+      @sub_total += pokemon.price
+    end
   end
 end
