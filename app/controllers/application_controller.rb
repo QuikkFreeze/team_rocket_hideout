6a19a447fdc8d23aee7dbe766a674367531261b6
Landon Soloway <quikkfreeze@gmail.com>
2020-04-13 14:28:01 -0500

    Reroute sign ins to previous page, and stopped devise from deleteing cart session on sign out.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :store_user_location!, if: :storable_location?
   before_action :all_types
   before_action :initialize_session
 
@@ -12,5 +13,13 @@ class ApplicationController < ActionController::Base
 
   def initialize_session
     session[:cart] ||= {}
+  end
+
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_user_location!
+    store_location_for(:user, request.fullpath)
   end
 end
