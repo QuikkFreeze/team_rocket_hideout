af78637757dd8bd59da65ef3f3d5e17fb548d6d4
Landon Soloway <quikkfreeze@gmail.com>
2020-04-13 16:31:31 -0500

    Setup address controller actions.

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,16 +1,59 @@
+# frozen_string_literal: true
+
 class AddressesController < ApplicationController
+  before_action :set_address, only: %i[show edit update]
+
+  # GET /tests/1
+  # GET /tests/1.json
+  def show; end
+
+  # GET /tests/new
   def new
+    @address = Address.new
   end
 
-  def edit
-  end
+  # GET /tests/1/edit
+  def edit; end
 
-  def show
-  end
-
+  # POST /tests
+  # POST /tests.json
   def create
+    @address = Address.new(address_params)
+
+    respond_to do |format|
+      if @address.save
+        format.html { redirect_to @address, notice: 'Address was successfully created.' }
+        format.json { render :show, status: :created, location: @address }
+      else
+        format.html { render :new }
+        format.json { render json: @address.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
+  # PATCH/PUT /tests/1
+  # PATCH/PUT /tests/1.json
   def update
+    respond_to do |format|
+      if @test.update(test_params)
+        format.html { redirect_to @test, notice: 'Test was successfully updated.' }
+        format.json { render :show, status: :ok, location: @test }
+      else
+        format.html { render :edit }
+        format.json { render json: @test.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_address
+    @address = Address.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def address_params
+    params.require(:address).permit(:name, :address, :city, :province_id)
   end
 end
